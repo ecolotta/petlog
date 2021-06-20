d7dded4c8c36fd1bbe1b7f3b86cc05e177b4511e
ecolotta <ecolotta@gmail.com>
2021-06-20 11:53:31 +0900

    愛犬未登録の場合に登録用リンクを送る_#4

--- app/controllers/api/linebot_controller.rb
+++ app/controllers/api/linebot_controller.rb
@@ -22,175 +22,182 @@ module Api
       events = client.parse_events_from(body) # jsonをインスタンスに変更
 
       events.each do |event|
+        user = Authentication.find_by(uid: event['source']['userId']).user
+        dog = user.dog
         case event
-        when Line::Bot::Event::Message # アカウント連携開始※ボタンなどに変更する
+        when Line::Bot::Event::Message 
           case event.message['text']
           when 'フレックス' # リッチメニューからお世話登録を選択した時に出る選択肢
-            message = {
-              "type": 'flex',
-              "altText": 'This is a Flex Message',
-              "contents": {
-                "type": 'bubble',
-                "body": {
-                  "type": 'box',
-                  "layout": 'vertical',
-                  "contents": [
-                    {
-                      "type": 'box',
-                      "layout": 'horizontal',
-                      "contents": [
-                        {
-                          "type": 'button',
-                          "action": {
-                            "type": 'postback',
-                            "label": 'ごはん',
-                            "data": 'care_type, 1'
+            if dog
+              message = {
+                "type": 'flex',
+                "altText": 'This is a Flex Message',
+                "contents": {
+                  "type": 'bubble',
+                  "body": {
+                    "type": 'box',
+                    "layout": 'vertical',
+                    "contents": [
+                      {
+                        "type": 'box',
+                        "layout": 'horizontal',
+                        "contents": [
+                          {
+                            "type": 'button',
+                            "action": {
+                              "type": 'postback',
+                              "label": 'ごはん',
+                              "data": 'care_type, 1'
+                            },
+                            "color": '#008000',
+                            "style": 'primary'
                           },
-                          "color": '#008000',
-                          "style": 'primary'
-                        },
-                        {
-                          "type": 'button',
-                          "action": {
-                            "type": 'postback',
-                            "label": 'おさんぽ',
-                            "data": 'care_type, 2'
+                          {
+                            "type": 'button',
+                            "action": {
+                              "type": 'postback',
+                              "label": 'おさんぽ',
+                              "data": 'care_type, 2'
+                            },
+                            "color": '#008000',
+                            "style": 'primary'
+                          }
+                        ],
+                        "paddingAll": 'sm',
+                        "spacing": 'md',
+                        "position": 'relative'
+                      },
+                      {
+                        "type": 'box',
+                        "layout": 'horizontal',
+                        "contents": [
+                          {
+                            "type": 'button',
+                            "action": {
+                              "type": 'postback',
+                              "label": 'おもちゃ',
+                              "data": 'care_type, 3'
+                            },
+                            "color": '#008000',
+                            "style": 'primary'
                           },
-                          "color": '#008000',
-                          "style": 'primary'
-                        }
-                      ],
-                      "paddingAll": 'sm',
-                      "spacing": 'md',
-                      "position": 'relative'
-                    },
-                    {
-                      "type": 'box',
-                      "layout": 'horizontal',
-                      "contents": [
-                        {
-                          "type": 'button',
-                          "action": {
-                            "type": 'postback',
-                            "label": 'おもちゃ',
-                            "data": 'care_type, 3'
+                          {
+                            "type": 'button',
+                            "action": {
+                              "type": 'postback',
+                              "label": 'グルーミング',
+                              "data": 'care_type, 4'
+                            },
+                            "color": '#008000',
+                            "style": 'primary'
+                          }
+                        ],
+                        "paddingAll": 'sm',
+                        "spacing": 'md',
+                        "position": 'relative'
+                      },
+                      {
+                        "type": 'box',
+                        "layout": 'horizontal',
+                        "contents": [
+                          {
+                            "type": 'button',
+                            "action": {
+                              "type": 'postback',
+                              "label": 'おくすり',
+                              "data": 'care_type, 5'
+                            },
+                            "color": '#008000',
+                            "style": 'primary'
                           },
-                          "color": '#008000',
-                          "style": 'primary'
-                        },
-                        {
-                          "type": 'button',
-                          "action": {
-                            "type": 'postback',
-                            "label": 'グルーミング',
-                            "data": 'care_type, 4'
+                          {
+                            "type": 'button',
+                            "action": {
+                              "type": 'postback',
+                              "label": 'おそうじ',
+                              "data": 'care_type, 6'
+                            },
+                            "color": '#008000',
+                            "style": 'primary'
+                          }
+                        ],
+                        "paddingAll": 'sm',
+                        "spacing": 'md',
+                        "position": 'relative'
+                      },
+                      {
+                        "type": 'box',
+                        "layout": 'horizontal',
+                        "contents": [
+                          {
+                            "type": 'button',
+                            "action": {
+                              "type": 'postback',
+                              "label": '芸の練習',
+                              "data": 'care_type, 7'
+                            },
+                            "color": '#008000',
+                            "style": 'primary'
                           },
-                          "color": '#008000',
-                          "style": 'primary'
-                        }
-                      ],
-                      "paddingAll": 'sm',
-                      "spacing": 'md',
-                      "position": 'relative'
-                    },
-                    {
-                      "type": 'box',
-                      "layout": 'horizontal',
-                      "contents": [
-                        {
-                          "type": 'button',
-                          "action": {
-                            "type": 'postback',
-                            "label": 'おくすり',
-                            "data": 'care_type, 5'
+                          {
+                            "type": 'button',
+                            "action": {
+                              "type": 'postback',
+                              "label": '見つめ合い',
+                              "data": 'care_type, 8'
+                            },
+                            "color": '#008000',
+                            "style": 'primary'
+                          }
+                        ],
+                        "paddingAll": 'sm',
+                        "spacing": 'md',
+                        "position": 'relative'
+                      },
+                      {
+                        "type": 'box',
+                        "layout": 'horizontal',
+                        "contents": [
+                          {
+                            "type": 'button',
+                            "action": {
+                              "type": 'postback',
+                              "label": '撫でた',
+                              "data": 'care_type, 9'
+                            },
+                            "color": '#008000',
+                            "style": 'primary'
                           },
-                          "color": '#008000',
-                          "style": 'primary'
-                        },
-                        {
-                          "type": 'button',
-                          "action": {
-                            "type": 'postback',
-                            "label": 'おそうじ',
-                            "data": 'care_type, 6'
-                          },
-                          "color": '#008000',
-                          "style": 'primary'
-                        }
-                      ],
-                      "paddingAll": 'sm',
-                      "spacing": 'md',
-                      "position": 'relative'
-                    },
-                    {
-                      "type": 'box',
-                      "layout": 'horizontal',
-                      "contents": [
-                        {
-                          "type": 'button',
-                          "action": {
-                            "type": 'postback',
-                            "label": '芸の練習',
-                            "data": 'care_type, 7'
-                          },
-                          "color": '#008000',
-                          "style": 'primary'
-                        },
-                        {
-                          "type": 'button',
-                          "action": {
-                            "type": 'postback',
-                            "label": '見つめ合い',
-                            "data": 'care_type, 8'
-                          },
-                          "color": '#008000',
-                          "style": 'primary'
-                        }
-                      ],
-                      "paddingAll": 'sm',
-                      "spacing": 'md',
-                      "position": 'relative'
-                    },
-                    {
-                      "type": 'box',
-                      "layout": 'horizontal',
-                      "contents": [
-                        {
-                          "type": 'button',
-                          "action": {
-                            "type": 'postback',
-                            "label": '撫でた',
-                            "data": 'care_type, 9'
-                          },
-                          "color": '#008000',
-                          "style": 'primary'
-                        },
-                        {
-                          "type": 'button',
-                          "action": {
-                            "type": 'postback',
-                            "label": 'その他',
-                            "data": 'care_type, 10'
-                          },
-                          "color": '#008000',
-                          "style": 'primary'
-                        }
-                      ],
-                      "paddingAll": 'sm',
-                      "spacing": 'md',
-                      "position": 'relative'
-                    }
-                  ]
+                          {
+                            "type": 'button',
+                            "action": {
+                              "type": 'postback',
+                              "label": 'その他',
+                              "data": 'care_type, 10'
+                            },
+                            "color": '#008000',
+                            "style": 'primary'
+                          }
+                        ],
+                        "paddingAll": 'sm',
+                        "spacing": 'md',
+                        "position": 'relative'
+                      }
+                    ]
+                  }
                 }
               }
-            }
+            else
+              message = {
+                type: 'text',
+                text: '愛犬を登録しましょう https://b0539a756602.ngrok.io/'
+              }
+            end
 
             client.reply_message(event['replyToken'], message)
           end
 
         when Line::Bot::Event::Postback # お世話ボタンが押された時にdog_careに保存する
-          user = Authentication.find_by(uid: event['source']['userId']).user
-          dog = user.dog
           if event['postback']['data'].include?('care_type')
             care_type_id = event['postback']['data'].split(',')[1].to_i
             dog.dog_cares.create!(care_type_id: care_type_id)
